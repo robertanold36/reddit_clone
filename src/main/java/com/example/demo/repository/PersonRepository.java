4e8a035f35e67549d7675150d7d7498a4f460d22
robertanold36 <robertanold70@gmail.com>
2020-11-17 17:20:51 +0300

    configure database connection

--- src/main/java/com/example/demo/repository/PersonRepository.java
+++ src/main/java/com/example/demo/repository/PersonRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PersonRepository extends JpaRepository<Long, Person> {
+public interface PersonRepository extends JpaRepository<Person,Long> {
 }
